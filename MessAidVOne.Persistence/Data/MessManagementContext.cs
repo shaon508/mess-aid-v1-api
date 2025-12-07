a8eea00383ef9732a79aea187990a61538ab1f6d
shaon-roy <shaonroy54@gmail.com>
2025-12-07 23:47:03 +0600

    remove connection string

--- MessAidVOne.Persistence/Data/MessManagementContext.cs
+++ MessAidVOne.Persistence/Data/MessManagementContext.cs
@@ -21,11 +21,7 @@ public partial class MessManagementContext : DbContext
     public virtual DbSet<ActivityInformation> ActivityInformations { get; set; }
 
     public virtual DbSet<UserActivity> UserActivities { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=mess_management;user=root;password=Shaon@cse508", ServerVersion.Parse("8.0.41-mysql"));
-
+   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
